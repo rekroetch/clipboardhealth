202fd6437112f2634c5800e1fd730e0713b44e2a
Rachel Kroetch <rekroetch@ucdavis.edu>
2022-11-23 13:22:11 -0800

    refactor update

--- Refactoring.md
+++ Refactoring.md
@@ -9,3 +9,5 @@ You've been asked to refactor the function `deterministicPartitionKey` in [`dpk.
 You will be graded on the exhaustiveness and quality of your unit tests, the depth of your refactor, and the level of insight into your thought process provided by the written explanation.
 
 ## Your Explanation Here
+
+I refactored the code to follow a more logical progression.  If the function receives no input, it returns the Trival_Partition_Key.  If anything at all is passed in, even an empty object, the function then handles that in order.  If a partitionKey is in the object, the candidate is assigned that value, then it is modified if it needs to be strigified or if it is longer than the Max_Partition_Key_Length.  Finally if an empty object is passed in, the canditated is assigned a random hash code. Then the function returns the candidate variable. 
--- dpk.test.js
+++ dpk.test.js
@@ -5,12 +5,12 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
-  it("Returns a random hex code when given an empty object", () => {
+  it("Returns a random hash code when given an empty object", () => {
     const trivialKey = deterministicPartitionKey({});
     expect(trivialKey).not.toBe("0");
     expect(trivialKey).toHaveLength(128);
   });
-  it("Returns a hex code when given an object without a partitionKey", () => {
+  it("Returns a hash code when given an object without a partitionKey", () => {
     const trivialKey = deterministicPartitionKey({test: 'jest'});
     expect(trivialKey).not.toBe("0");
     expect(trivialKey).toHaveLength(128);
@@ -24,7 +24,7 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe('12');
     expect(trivialKey).not.toBe(12);
   });
-  it("Returns hex code when partitionKey has length > 256", () => {
+  it("Returns hash code when partitionKey has length > 256", () => {
     const trivialKey = deterministicPartitionKey({partitionKey: 'abd327134621195ab0ff7904437093be0a27584d3b3edc7249eeca6aefc9ae3837028b34e921cbad6cf5c1f89b95dc9a445bcc3e2851bbda0b9d0f102a7bbbdcabd327134621195ab0ff7904437093be0a27584d3b3edc7249eeca6aefc9ae3837028b34e921cbad6cf5c1f89b95dc9a445bcc3e2851bbda0b9d0f102a7bbbdct'});
     console.log(trivialKey)
     expect(trivialKey).toHaveLength(128);
